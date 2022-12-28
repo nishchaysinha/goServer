65c8f05ea1b771c83747aca29518b0cff4990194
Nishchay Sinha <nishchay.space@gmail.com>
2022-12-28 18:16:45 +0530

    🤓

--- main.go
+++ main.go
@@ -32,11 +32,26 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/test" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Printf("Laude")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/test", testHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
